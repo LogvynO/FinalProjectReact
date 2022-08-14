7c17ff832e7928563dacf59aa5ab2253628d9f7e
“LogvynO” <logvyn.o@gmail.com>
2022-08-14 15:09:51 +0200

    last update

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -47,7 +47,7 @@ const ChatFeed = (props) => {
   return (
     <div className="chat-feed">
       <div className="chat-title-container">
-        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-title">{chat.title}</div>
         <div className="chat-subtitle">
           {chat.people.map((person) => ` ${person.person.username}`)}
         </div>
